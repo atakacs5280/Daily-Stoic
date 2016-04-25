90716600280950423e57c894ea0c6572c8d41734
Alex Takacs <alexander.g.takacs@gmail.com>
2016-04-25 14:38:20 -0600

    its alive

--- dailyStoic/src/index.js
+++ dailyStoic/src/index.js
@@ -9,40 +9,95 @@
 */
 
 /**
- * This sample shows how to create a Lambda function for handling Alexa Skill requests that:
- *
- * - Multiple slots: has 2 slots (name and score)
- * - Database Interaction: demonstrates how to read and write data to DynamoDB.
- * - NUMBER slot: demonstrates how to handle number slots.
- * - Custom slot type: demonstrates using custom slot types to handle a finite set of known values
- * - Dialog and Session state: Handles two models, both a one-shot ask and tell model, and a multi-turn dialog model.
- *   If the user provides an incorrect slot in a one-shot model, it will direct to the dialog model. See the
- *   examples section for sample interactions of these models.
+ * This simple sample has no external dependencies or session management, and shows the most basic
+ * example of how to create a Lambda function for handling Alexa Skill requests.
  *
  * Examples:
- * Dialog model:
- *  User: "Alexa, tell score keeper to reset."
- *  Alexa: "New game started without players. Who do you want to add first?"
- *  User: "Add the player Bob"
- *  Alexa: "Bob has joined your game"
- *  User: "Add player Jeff"
- *  Alexa: "Jeff has joined your game"
- *
- *  (skill saves the new game and ends)
- *
- *  User: "Alexa, tell score keeper to give Bob three points."
- *  Alexa: "Updating your score, three points for Bob"
- *
- *  (skill saves the latest score and ends)
- *
  * One-shot model:
- *  User: "Alexa, what's the current score?"
- *  Alexa: "Jeff has zero points and Bob has three"
+ *  User: "Alexa, ask Space Geek for a space fact"
+ *  Alexa: "Here's your space fact: ..."
  */
-'use strict';
-var ScoreKeeper = require('./scoreKeeper');
 
+/**
+ * App ID for the skill
+ */
+var APP_ID = 'arn:aws:lambda:us-east-1:865113720107:function:dailyStoic'; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+
+/**
+ * The AlexaSkill prototype and helper functions
+ */
+var AlexaSkill = require('./AlexaSkill');
+var storage = require('./storage');
+
+/**
+ * SpaceGeek is a child of AlexaSkill.
+ * To read more about inheritance in JavaScript, see the link below.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
+ */
+var DailyStoic = function () {
+    AlexaSkill.call(this, APP_ID);
+};
+
+// Extend AlexaSkill
+DailyStoic.prototype = Object.create(AlexaSkill.prototype);
+DailyStoic.prototype.constructor = DailyStoic;
+
+DailyStoic.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+    console.log("DailyStoic onSessionStarted requestId: " + sessionStartedRequest.requestId
+        + ", sessionId: " + session.sessionId);
+    // any initialization logic goes here
+};
+
+DailyStoic.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    console.log("DailyStoic onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+    handleNewFactRequest(session, response);
+};
+
+/**
+ * Overridden to show that a subclass can override this function to teardown session state.
+ */
+DailyStoic.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+    console.log("DailyStoic onSessionEnded requestId: " + sessionEndedRequest.requestId
+        + ", sessionId: " + session.sessionId);
+    // any cleanup logic goes here
+};
+
+DailyStoic.prototype.intentHandlers = {
+    "GetNewFactIntent": function (intent, session, response) {
+        handleNewFactRequest(session, response);
+    },
+
+    "AMAZON.HelpIntent": function (intent, session, response) {
+        response.ask("You can ask Daily Stoic to tell me a quote from stoic philosophy, or, you can say exit... What can I help you with?", "What can I help you with?");
+    },
+
+    "AMAZON.StopIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
+    },
+
+    "AMAZON.CancelIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
+    }
+};
+
+/**
+ * Gets a random new fact from the list and returns to the user.
+ */
+function handleNewFactRequest(session, response) {
+    storage.loadGame(session, function (currentQuote) {
+      // Create speech output
+      var speechOutput = "Here's your Daily Stoic quote: " + currentQuote;
+
+      response.tellWithCard(speechOutput, "Daily Stoic", speechOutput);
+    });
+}
+
+// Create the handler that responds to the Alexa Request.
 exports.handler = function (event, context) {
-    var scoreKeeper = new ScoreKeeper();
-    scoreKeeper.execute(event, context);
+    // Create an instance of the DailyStoic skill.
+    var dailyStoic = new DailyStoic();
+    dailyStoic.execute(event, context);
 };
--- dailyStoic/src/storage.js
+++ dailyStoic/src/storage.js
@@ -10,99 +10,107 @@
 
 'use strict';
 var AWS = require("aws-sdk");
+var moment = require('moment');
 
 var storage = (function () {
     var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
-
-    /*
-     * The Game class stores all game states for the user
-     */
-    function Game(session, data) {
-        if (data) {
-            this.data = data;
-        } else {
-            this.data = {
-                players: [],
-                scores: {}
-            };
-        }
-        this._session = session;
-    }
-
-    Game.prototype = {
-        isEmptyScore: function () {
-            //check if any one had non-zero score,
-            //it can be used as an indication of whether the game has just started
-            var allEmpty = true;
-            var gameData = this.data;
-            gameData.players.forEach(function (player) {
-                if (gameData.scores[player] !== 0) {
-                    allEmpty = false;
-                }
-            });
-            return allEmpty;
-        },
-        save: function (callback) {
-            //save the game states in the session,
-            //so next time we can save a read from dynamoDB
-            this._session.attributes.currentGame = this.data;
-            dynamodb.putItem({
-                TableName: 'ScoreKeeperUserData',
-                Item: {
-                    CustomerId: {
-                        S: this._session.user.userId
-                    },
-                    Data: {
-                        S: JSON.stringify(this.data)
-                    }
-                }
-            }, function (err, data) {
-                if (err) {
-                    console.log(err, err.stack);
-                }
-                if (callback) {
-                    callback();
-                }
-            });
-        }
-    };
-
+    var todayIS = moment().subtract(5, 'hours').format('l');
+    console.log(todayIS);
+//     /*
+//      * The Game class stores all game states for the user
+//      */
+//     function Game(session, data) {
+//         if (data) {
+//             this.data = data;
+//         } else {
+//             this.data = {
+//                 players: [],
+//                 scores: {}
+//             };
+//         }
+//         this._session = session;
+//     }
+//
+    // Game.prototype = {
+//         isEmptyScore: function () {
+//             //check if any one had non-zero score,
+//             //it can be used as an indication of whether the game has just started
+//             var allEmpty = true;
+//             var gameData = this.data;
+//             gameData.players.forEach(function (player) {
+//                 if (gameData.scores[player] !== 0) {
+//                     allEmpty = false;
+//                 }
+//             });
+//             return allEmpty;
+//         },
+//         save: function (callback) {
+//             //save the game states in the session,
+//             //so next time we can save a read from dynamoDB
+//             this._session.attributes.currentGame = this.data;
+//             dynamodb.putItem({
+//                 TableName: 'ScoreKeeperUserData',
+//                 Item: {
+//                     CustomerId: {
+//                         S: this._session.user.userId
+//                     },
+//                     Data: {
+//                         S: JSON.stringify(this.data)
+//                     }
+//                 }
+//             }, function (err, data) {
+//                 if (err) {
+//                     console.log(err, err.stack);
+//                 }
+//                 if (callback) {
+//                     callback();
+//                 }
+//             });
+//         }
+    // };
+//
     return {
-        loadGame: function (session, callback) {
-            if (session.attributes.currentGame) {
-                console.log('get game from session=' + session.attributes.currentGame);
-                callback(new Game(session, session.attributes.currentGame));
-                return;
-            }
+        loadGame: function(session, callback) {
+            // if (session.attributes.currentGame) {
+            //     console.log('get game from session=' + session.attributes.currentGame);
+            //     callback(new Game(session, session.attributes.currentGame));
+            //     return;
+            // }
+            console.log(todayIS);
             dynamodb.getItem({
-                TableName: 'ScoreKeeperUserData',
+                TableName: 'StoicQuotes',
                 Key: {
-                    CustomerId: {
-                        S: session.user.userId
+                    "TodayIs": {
+                      "S": todayIS
                     }
                 }
-            }, function (err, data) {
-                var currentGame;
+            },
+
+                function (err, data) {
+                console.log(data);
+                var currentQuote = '' ;
                 if (err) {
                     console.log(err, err.stack);
-                    currentGame = new Game(session);
-                    session.attributes.currentGame = currentGame.data;
-                    callback(currentGame);
-                } else if (data.Item === undefined) {
-                    currentGame = new Game(session);
-                    session.attributes.currentGame = currentGame.data;
-                    callback(currentGame);
+                    // currentQuote = new Game(session);
+                    // session.attributes.currentQuote = currentQuote.data;
+                    callback(currentQuote);
+                } else if (data === undefined) {
+                    console.log('else if (data === undefined) {' + data);
+                    // currentQuote = new Game(session);
+                    // session.attributes.currentQuote = currentQuote.data;
+                    callback(currentQuote);
                 } else {
-                    console.log('get game from dynamodb=' + data.Item.Data.S);
-                    currentGame = new Game(session, JSON.parse(data.Item.Data.S));
-                    session.attributes.currentGame = currentGame.data;
-                    callback(currentGame);
+                    console.log('get quote from dynamodb=' + data.Item.Quote.S);
+                    //currentQuote = JSON.parse(data.Item.Quote.S);
+                    session.attributes.currentQuote = data.Item.Quote.S;
+                    currentQuote = session.attributes.currentQuote;
+                    callback(currentQuote);
                 }
             });
-        },
-        newGame: function (session) {
-            return new Game(session);
         }
+//         newGame: function (session) {
+//             return new Game(session);
+//         }
     };
 })();
 module.exports = storage;
